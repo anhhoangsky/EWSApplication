c4d3046e8059958fe8b9a58a0279edca46ffdf66
BinhLeo <votrannhatbinh1999@gmail.com>
2021-02-27 13:37:39 +0700

    code Tags

--- EWSApplication.DataLayers/ManagerDAL.cs
+++ EWSApplication.DataLayers/ManagerDAL.cs
@@ -11,6 +11,47 @@ namespace EWSApplication.DataLayers
     {
         EWSDbContext db = new EWSDbContext();
         #region Tags
+        /// <summary>
+        /// Tao Tag moi
+        /// </summary>
+        /// <param name="tagName"></param>
+        /// <returns></returns>
+        public bool CreateNewTag(string tagName)
+        {
+            try
+            {
+                var tag = new Tag()
+                {
+                    tagname = tagName
+                };
+                db.Tags.Add(tag);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Xoa tag
+        /// </summary>
+        /// <param name="tagID"></param>
+        /// <returns></returns>
+        public bool DeleteTag(string tagID)
+        {
+            try
+            {
+                var tag = db.Tags.Where(x => x.tagid == tagID).SingleOrDefault();
+                db.Tags.Remove(tag);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
         #endregion
     }
 }
