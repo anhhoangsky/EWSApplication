c19c192ca096f62f4e339c0c0dab1a6405651173
anhsky <an38570@gmail.com>
2021-03-06 16:25:57 +0700

    fix comment anonymous

--- EWSApplication/Controllers/PostController.cs
+++ EWSApplication/Controllers/PostController.cs
@@ -54,7 +54,7 @@ namespace EWSApplication.Controllers
                 if (file != null)
                 {
                     var fileName = Path.GetFileName(file.FileName);
-                    filePath = Path.Combine(Server.MapPath("/Files"), fileName);
+                    filePath = Path.Combine(Server.MapPath("/Files"));
                     file.SaveAs(filePath);
                 }
                 else
